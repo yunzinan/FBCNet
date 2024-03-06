24b1d3c354604477335f7bd608c37e61aadbfdda
yunzinan <shen.ouy03@gmail.com>
2024-03-06 22:09:31 +0800

    new bugs

--- codes/classify/fbcnet.py
+++ codes/classify/fbcnet.py
@@ -4,17 +4,67 @@ define a class as the wrapper of FBCNet
 """
 
 import ho as HO 
+from ho import networks
+from ho import baseModel
+import numpy as np
+import torch
+
+config = {}
+
+config['randSeed'] = 20190821
+
+config['batchSize'] = 30
+
+
+config['modelArguments'] = {'nChan': 14, 'nTime': 250, 'dropoutP': 0.5,
+                                    'nBands':9, 'm' : 32, 'temporalLayer': 'LogVarLayer',
+                                    'nClass':3, 'doWeightNorm': True}
+
+
+# Training related details    
+config['modelTrainArguments'] = {'stopCondi':  {'c': {'Or': {'c1': {'MaxEpoch': {'maxEpochs': 1500, 'varName' : 'epoch'}},
+          'c2': {'NoDecrease': {'numEpochs' : 200, 'varName': 'valInacc'}} } }},
+          'classes': [0,1,2], 'sampler' : 'RandomSampler', 'loadBestModel': True,
+          'bestVarToCheck': 'valInacc', 'continueAfterEarlystop':True,'lr': 1e-3}
 
 
 class FBCNet:
     def __init__(self):
+        self.network = networks.__dict__['FBCNet']
+        self.device = torch.device("cuda")
+        self.setRandom(config['randSeed']) 
         self.loadModel()
+
+    def setRandom(self, seed):
+        '''
+        Set all the random initializations with a given seed
+
+        '''
+        # Set np
+        np.random.seed(seed)
+
+        # Set torch
+        torch.manual_seed(seed)
+
+        # Set cudnn
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+    def count_parameters(self):
+        return sum(p.numel() for p in self.net.parameters() if p.requires_grad)
 
     def loadModel(self):
         """
         Load the pre-trained model state dict. 
         """
         print("loading the pre-trained model.")
+        self.net = self.network(**config['modelArguments'])
+        print('Trainable Parameters in the network are: ' + str(self.count_parameters()))
+        loaded_state_dict = torch.load('models/model_state_dict.pth')
+        self.net.load_state_dict(loaded_state_dict)
+        print("pre-trained model loaded.")
+        self.model = baseModel(net=self.net, resultsSavePath=None, batchSize=config['batchSize'])
+        self.model
 
     def finetune(self, data):
         """
@@ -32,6 +82,7 @@ class FBCNet:
         Nothing. but the model state dict will be updated.
         """ 
         print("the model will be finetuned.") 
+        # self.model.train()
 
     def inference(self, data):
         """
@@ -48,8 +99,14 @@ class FBCNet:
         - label: int 
             the predicted label for the input data
         """
-        print("now the data will be inferenced")
+        # print("now the data will be inferenced")
+        d = torch.from_numpy(data).unsqueeze(1)
+        with torch.no_grad():
+            preds = self.net(d.to(self.device))
 
+            _, preds = torch.max(preds, 1)
+
+        return preds.cpu().numpy()
 
 
 
--- codes/classify/test.py
+++ codes/classify/test.py
@@ -1,12 +1,115 @@
 import fbcnet
 
+import numpy as np
+import torch
+import sys
+import os
+import time
+import xlwt
+import csv
+import random
+import math
+import copy
+from torch.utils.data import DataLoader
+
+masterPath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(1, os.path.join(masterPath, 'centralRepo'))
+from eegDataset import eegDataset
+from baseModel import baseModel
+import networks
+import transforms
+from saveData import fetchData
+
+
+
+
+def get_data():
+    """
+    fetch the lyh dataset 
+    """
+    config = {}
+
+    toolboxPath = os.path.dirname(masterPath)
+    config['inDataPath'] = os.path.join(toolboxPath, 'data')
+
+    modeInFol = 'multiviewPython'
+
+    config['inDataPath'] = os.path.join(config['inDataPath'], 'lyh', modeInFol)
+
+    # Path to the input data labels file
+    config['inLabelPath'] = os.path.join(config['inDataPath'], 'dataLabels.csv')
+
+    fetchData(os.path.dirname(config['inDataPath']), 2) # Make sure that all the required data is present!
+    print("Data loading finished")
+    data = eegDataset(dataPath = config['inDataPath'], dataLabelsPath= config['inLabelPath'], preloadData = False, transform=None)
+
+        
+    trainData = copy.deepcopy(data)
+    testData = copy.deepcopy(data)
+
+    if len(data.labels[0])>4:
+        idxTrain = [i for i, x in enumerate(data.labels) if x[4] == '0' ]
+        idxTest = [i for i, x in enumerate(data.labels) if x[4] == '1' ]
+    else:
+        raise ValueError("The data can not be divided based on the sessions")
+
+    testData.createPartialDataset(idxTest)
+    finetuneData = copy.deepcopy(testData)
+    finetuneData.createPartialDataset(list(range(30)))
+    testData.createPartialDataset(list(range(30, len(testData))))
+    trainData.createPartialDataset(idxTrain)
+
+    
+
+    return trainData, finetuneData, testData
+
+    return data
+
 
 if __name__ == "__main__":
     fbcnet = fbcnet.FBCNet()
 
-    fbcnet.loadModel()
+    trainData, finetuneData, testData = get_data()
 
-    fbcnet.finetune()
+    print("------train set w/o finetune----------")
+    dataLoader = DataLoader(trainData, batch_size=1, shuffle=False)
 
-    fbcnet.inference()
+    ans_list = []
+    tot_cnt = 0
+    acc_cnt = 0
+
+    for d in dataLoader:
+        data = d['data'].numpy()
+        label = d['label'].numpy()
+        # print(data.shape)
+        l = fbcnet.inference(data) 
+        tot_cnt += 1
+        if l == label:
+            acc_cnt += 1
+        ans_list.append(l[0])
+
+    print(f"tot: {tot_cnt} correct: {acc_cnt} acc: {acc_cnt / tot_cnt}")
+
+    print("------test set w/o finetune----------")
+    dataLoader = DataLoader(testData, batch_size=1, shuffle=False)
+
+    ans_list = []
+    tot_cnt = 0
+    acc_cnt = 0
+
+    for d in dataLoader:
+        data = d['data'].numpy()
+        label = d['label'].numpy()
+        # print(data.shape)
+        l = fbcnet.inference(data) 
+        tot_cnt += 1
+        if l == label:
+            acc_cnt += 1
+        ans_list.append(l[0])
+
+    print(f"tot: {tot_cnt} correct: {acc_cnt} acc: {acc_cnt / tot_cnt}")
+    
+    # fbcnet.finetune()
+
+    # fbcnet.inference()
     
